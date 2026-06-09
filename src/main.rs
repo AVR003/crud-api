926ed95be8587d8f56fad9ac842def4dfc6f178d
AVR003 <aakashvr93@gmail.com>
2026-06-09 11:58:18 +0530

    Prepare API for Render deployment

--- src/main.rs
+++ src/main.rs
@@ -118,8 +118,10 @@ async fn main() {
         .route("/students/{id}", delete(delete_student))
         .with_state(state);
     
-    let listener=
-        tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+
+    let listener = tokio::net::TcpListener::bind(addr)
         .await
         .unwrap();
 
